585c6b62410c04c8e4d99127f9bfcf0cc128465b
HHH <215681197@qq.com>
2018-11-15 15:12:31 +0800

    tensorboard 可视化2

--- tf_build_network.py
+++ tf_build_network.py
@@ -9,20 +9,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def add_layer(inputs, in_size, out_size, activation_function=None):
+def add_layer(inputs, in_size, out_size, n_layer,activation_function=None):
     # 添加层数并且返回这层的output
-    with tf.name_scope('layer'):
-        with tf.name_scope('weights'):
-           weights = tf.Variable(tf.random_normal([in_size, out_size]),name='W')
+    layer_name = 'layer%s'%n_layer
+    with tf.name_scope(layer_name):
+        with tf.name_scope('layer'):
+            with tf.name_scope('weights'):
+                weights = tf.Variable(tf.random_normal([in_size, out_size]), name='W')
+                tf.summary.histogram(layer_name + '/weights', weights)
         with tf.name_scope('biase'):
-           biases = tf.Variable(tf.zeros([1, out_size]) + 0.1,name='b')
+            biases = tf.Variable(tf.zeros([1, out_size]) + 0.1, name='b')
+            tf.summary.histogram(layer_name + '/biases', biases)
         with tf.name_scope('Wx_plus_b'):
-           Wx_plus_b = tf.matmul(inputs, weights) + biases
+            Wx_plus_b = tf.matmul(inputs, weights) + biases
         if activation_function is None:
             outputs = Wx_plus_b
+            tf.summary.histogram(layer_name + '/outputs', outputs)
         else:
             outputs = activation_function(Wx_plus_b)
         return outputs
+
 
 # 构造数据
 x_data = np.linspace(-1, 1, 300)[:, np.newaxis]
@@ -35,17 +41,19 @@ with tf.name_scope('input'):
   xs = tf.placeholder(tf.float32, [None, 1],name='x_input')
   ys = tf.placeholder(tf.float32, [None, 1],name='y_input')
 # 添加隐藏层
-l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
+l1 = add_layer(xs, 1, 10, n_layer=1,activation_function=tf.nn.relu)
 # 添加输出层
-prediction = add_layer(l1, 10, 1, activation_function=None)
+prediction = add_layer(l1, 10, 1, n_layer=2,activation_function=None)
 # loss
 with tf.name_scope('loss'):
    loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction), reduction_indices=[1]),name='loss')
+   tf.summary.scalar('loss',loss)
 with tf.name_scope('train'):
    train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 
 init = tf.global_variables_initializer()
 sess = tf.Session()
+merged = tf.summary.merge_all()
 writer = tf.summary.FileWriter("./logs",sess.graph)
 # activate tensorflow 后到logs
 # 上一级 tensorboard --logdir=logs
@@ -62,7 +70,9 @@ plt.show()
 for i in range(1000):
     sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
     if i % 50 == 0:
-        # print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
+        result = sess.run(merged,
+                          feed_dict={xs: x_data, ys: y_data})
+        writer.add_summary(result,i)
         try:
           ax.lines.remove(lines[0])
         except Exception:
