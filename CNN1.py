04719b7c5eaae729ac1fdf75f7ea72d78ca9c290
HHH <215681197@qq.com>
2018-11-19 17:14:05 +0800

    三个卷积层1个全连接一层softmax输出 1 input_image(28*28*1) 2 conv1(5,5,1,32) 3 max_pool1(2*2)   output: 14*14*32 4 conv2(5,5,32,64) 5 max_pool2(2*2)   output:(7*7*64) 7 conv3(3,3,64,128) 8 max_pool(2*2)   output:(4*4*128) 9 fc(4*4*128,2048) 10 softmax(2048,10) step: 1000  acc:0.966

--- CNN1.py
+++ CNN1.py
@@ -73,16 +73,10 @@ cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction),
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
 sess = tf.Session()
-# important step
-# tf.initialize_all_variables() no long valid from
-# 2017-03-02 if using tensorflow >= 0.12
-if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:
-    init = tf.initialize_all_variables()
-else:
-    init = tf.global_variables_initializer()
+init = tf.global_variables_initializer()
 sess.run(init)
 
-for i in range(1000):
+for i in range(10000):
     batch_xs, batch_ys = mnist.train.next_batch(100)
     sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys, keep_prob: 0.5})
     if i % 50 == 0:
